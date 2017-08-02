cf276d503490ab9fcae50b6f8458a9da9f78a84c
Pipl-10 <pipl-10@Pipl-10s-iMac.local>
2017-08-02 14:44:00 +0530

    Initial Commit

---
+++ gmailLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gmailLogin
+//
+//  Created by Pipl-10 on 02/08/17.
+//  Copyright © 2017 Pipl-10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
